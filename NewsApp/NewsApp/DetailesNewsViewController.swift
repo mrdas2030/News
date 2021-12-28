b1c18feb407d6cecdf22ef6ef6f69dbc97acd8b7
YOUSEF ALMALKI <mmrdas600@gmail.com>
2021-12-28 16:54:11 +0300

    API has been added

---
+++ NewsApp/NewsApp/DetailesNewsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DetailesNewsViewController.swift
+//  NewsApp
+//
+//  Created by يوسف جابر المالكي on 24/05/1443 AH.
+//
+
+import UIKit
+import WebKit
+class DetailesNewsViewController: UIViewController {
+    var urlLink : String?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        wepView.load(NSURLRequest(url: NSURL(string: urlLink!)! as URL) as URLRequest)
+        if NSURL(string: urlLink!) != nil{
+          wepView.load(NSURLRequest(url: NSURL(string: urlLink!)!as URL)as URLRequest)
+        }else{
+            print("URL not found")
+        }
+    }
+    
+    @IBOutlet weak var wepView: WKWebView!
+    
+
+}
